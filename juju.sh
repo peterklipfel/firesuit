0f3c808160f83785c37cce90876ea82e0e661fbe
Peter Klipfel <peter@klipfel.me>
2013-09-10 00:34:04 +0000

    stands up storm architecture

--- juju.sh
+++ juju.sh
@@ -1,9 +1,9 @@
 $codename = cat /etc/lsb-release | grep CODENAME | cut -f2 -d'='
 
-sudo apt-get install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:juju/stable
 sudo apt-get update
-sudo apt-get install juju-core lxc mongodb-server
+sudo apt-get -y install juju-core lxc mongodb-server
 
 mkdir -p ~/charm/$codename
 git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
@@ -11,4 +11,9 @@ git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
 sed s/notsosecret/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ local.yaml > environments.yaml
 mkdir ~/.juju
 mv environments.yaml ~/.juju/environments.yaml
+
+sudo juju bootstrap
+juju deploy zookeeper
+juju deploy -v --repository=~/charm/ local:storm stormmaster
+juju deploy -v --repository=~/charm/ local:storm stormworker
 
