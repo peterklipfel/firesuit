441b0dbd8293f8a9457d054a1eb44e7b184eef27
Peter Klipfel <peter@klipfel.me>
2013-09-10 02:27:57 +0000

    hack for finding the right local charm

--- juju.sh
+++ juju.sh
@@ -6,7 +6,10 @@ sudo apt-get update
 sudo apt-get -y install juju-core lxc mongodb-server
 
 mkdir -p ~/charm/$codename
+mkdir -p ~/charm/precise
 git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
+git clone git@github.com:peterklipfel/storm_charm.git ~/charm/precise/storm
+
 
 sed s/notsosecret/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ local.yaml > environments.yaml
 mkdir ~/.juju
