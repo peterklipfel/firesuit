eebe4fe5d761a4fc092e5f3e84eb93c3005ce26d
Peter Klipfel <peter@klipfel.me>
2013-10-04 00:01:26 +0000

    testing unique strings for yaml

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 firesuit
 ========
 
+
+For Amazon
+----------
+This will spin up a couple instances
+
+
 _A framework for standing up big data applications_
 
 This project is my undergraduate senior project.  It aims to provide an opinionated boilerplate and framework for setting up data applications.  I have been working with big data tools for a relatively short amount of time, but have noticed that people are often repeating the same patterns.  Nathan Marz had the right idea with a real-time complex event processing unit, and a batch processing unit that picked up afterward.  However, the ecosystem is rapidly growing and expanding.  It would be nice to have a framework that allowed people to switch out functional layers and retain functionality.  Similar to the way that web frameworks provide an opinionated way for people to organize the tools of their choice (javascript libraries, databases, etc.).
---
+++ aws.sh
@@ -0,0 +1,22 @@
+#codename=`cat /etc/lsb-release | grep CODENAME | cut -f2 -d'='`
+
+#sudo apt-get -y install software-properties-common
+#sudo add-apt-repository ppa:juju/stable
+#sudo apt-get update
+#sudo apt-get -y install juju-core lxc mongodb-server
+
+#mkdir -p ~/charm/$codename
+#mkdir -p ~/charm/precise
+#git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
+#git clone git@github.com:peterklipfel/storm_charm.git ~/charm/precise/storm
+
+
+sed s/notsosecret/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ aws.yaml > aws.yaml
+sed s/notsouniqe/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ aws.yaml > environments.yaml
+#mkdir ~/.juju
+#mv environments.yaml ~/.juju/environments.yaml
+
+#sudo juju bootstrap
+#juju deploy zookeeper
+#juju deploy -v --repository=/home/$USER/charm/ local:storm stormmaster
+#juju deploy -v --repository=/home/$USER/charm/ local:storm stormworker
