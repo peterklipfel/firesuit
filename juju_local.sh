40c230c0fd74fd7fd35443a77ac5033b196e0fb0
Peter Klipfel <peter@klipfel.me>
2014-03-04 11:46:53 -0700

    juju relations between rabbitmq and flask initialize

--- juju_local.sh
+++ juju_local.sh
@@ -1,19 +1,31 @@
 codename=`cat /etc/lsb-release | grep CODENAME | cut -f2 -d'='`
 
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository ppa:juju/stable
-sudo apt-get update
-sudo apt-get -y install juju-core lxc mongodb-server
+# sudo apt-get -y install software-properties-common
+# if [[ `grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep juju/stable` ]]; then
+#   echo 'ppa has already been added'
+# else
+#   echo 'adding ppa'
+#   sudo add-apt-repository ppa:juju/stable
+# fi
 
-mkdir -p ~/charm/$codename
-mkdir -p ~/charm/precise
-git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
-git clone git@github.com:peterklipfel/storm_charm.git ~/charm/precise/storm
+# sudo apt-get update
+# if [[ "$codename" == "precise" ]]; then
+#   sudo apt-get install juju-local linux-image-generic-lts-raring linux-headers-generic-lts-raring
+# else
+#   sudo apt-get install juju-local
+# fi
+
+# mkdir -p ~/charm/$codename
+# mkdir -p ~/charm/precise
+# git clone git@github.com:peterklipfel/storm_charm.git ~/charm/$codename/storm
+# git clone git@github.com:peterklipfel/storm_charm.git ~/charm/precise/storm
 
 
-sed s/notsosecret/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ local.yaml > environments.yaml
-mkdir ~/.juju
-mv environments.yaml ~/.juju/environments.yaml
+# sed s/notsosecret/`tr -dc "[:alpha:]" < /dev/urandom | head -c 30`/ local.yaml > environments.yaml
+# mkdir ~/.juju
+# mv environments.yaml ~/.juju/environments.yaml
+
+juju switch local
 
 sudo juju bootstrap
 juju deploy zookeeper
